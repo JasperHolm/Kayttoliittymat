953b1aa76bc1d99100deab7bc1643a191cc0553b
JasperHolm <64134491+JasperHolm@users.noreply.github.com>
2020-10-29 11:04:38 +0200

    Add files via upload

---
+++ 08ArrayLotto/08ArrayLotto/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace _08ArrayLotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox6 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox7 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox8 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox9 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 139);
+            this.label1.MaximumSize = new System.Drawing.Size(10000, 10000);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Arvo numerot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(43, 40);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox2.TabIndex = 3;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(74, 40);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox3.TabIndex = 4;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(106, 40);
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox4.TabIndex = 5;
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.Location = new System.Drawing.Point(137, 40);
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox5.TabIndex = 6;
+            // 
+            // maskedTextBox6
+            // 
+            this.maskedTextBox6.Location = new System.Drawing.Point(168, 40);
+            this.maskedTextBox6.Name = "maskedTextBox6";
+            this.maskedTextBox6.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox6.TabIndex = 7;
+            // 
+            // maskedTextBox7
+            // 
+            this.maskedTextBox7.Location = new System.Drawing.Point(199, 40);
+            this.maskedTextBox7.Name = "maskedTextBox7";
+            this.maskedTextBox7.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox7.TabIndex = 8;
+            // 
+            // maskedTextBox8
+            // 
+            this.maskedTextBox8.Location = new System.Drawing.Point(230, 40);
+            this.maskedTextBox8.Name = "maskedTextBox8";
+            this.maskedTextBox8.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox8.TabIndex = 9;
+            // 
+            // maskedTextBox9
+            // 
+            this.maskedTextBox9.Location = new System.Drawing.Point(261, 40);
+            this.maskedTextBox9.Name = "maskedTextBox9";
+            this.maskedTextBox9.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox9.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 304);
+            this.Controls.Add(this.maskedTextBox9);
+            this.Controls.Add(this.maskedTextBox8);
+            this.Controls.Add(this.maskedTextBox7);
+            this.Controls.Add(this.maskedTextBox6);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lotto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox7;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox8;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox9;
+    }
+}
+
---
+++ 08ArrayLotto/08ArrayLotto/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08ArrayLotto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static bool Sama(int[] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value) return true;
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int koko = 9;
+            int[] lottonumerot = new int[koko];            
+
+            Random rand = new Random();
+
+            for (int i = 0; i < lottonumerot.Length; i++)
+            {
+                var seur = 0;
+                while (true)
+                {
+                    seur = rand.Next(10);
+                    if (!Sama(lottonumerot, seur)) break;
+                }
+                lottonumerot[i] = seur;
+
+                label1.Text = lottonumerot[0].ToString() + ", " + lottonumerot[1].ToString() + ", " + lottonumerot[2].ToString() + ", " + lottonumerot[3].ToString() + ", " + lottonumerot[4].ToString() + ", " + lottonumerot[5].ToString() + ", " + lottonumerot[6].ToString() + ", " + lottonumerot[7].ToString() + ", " + lottonumerot[8].ToString();
+
+                if (maskedTextBox1.Text == lottonumerot[0].ToString())
+                {
+                    MessageBox.Show("Veikkaus 1: Oikein");
+                }
+                else if (maskedTextBox2.Text == lottonumerot[1].ToString())
+                {
+                    MessageBox.Show("Veikkaus 2: Oikein");
+                }
+                else if (maskedTextBox3.Text == lottonumerot[2].ToString())
+                {
+                    MessageBox.Show("Veikkaus 3: Oikein");
+                }
+                else if (maskedTextBox4.Text == lottonumerot[3].ToString())
+                {
+                    MessageBox.Show("Veikkaus 4: Oikein");
+                }
+                else if (maskedTextBox5.Text == lottonumerot[4].ToString())
+                {
+                    MessageBox.Show("Veikkaus 5: Oikein");
+                }
+                else if (maskedTextBox6.Text == lottonumerot[5].ToString())
+                {
+                    MessageBox.Show("Veikkaus 6: Oikein");
+                }
+                else if (maskedTextBox7.Text == lottonumerot[6].ToString())
+                {
+                    MessageBox.Show("Veikkaus 7: Oikein");
+                }
+                else if (maskedTextBox8.Text == lottonumerot[7].ToString())
+                {
+                    MessageBox.Show("Veikkaus 8: Oikein");
+                }
+                else if (maskedTextBox9.Text == lottonumerot[8].ToString())
+                {
+                    MessageBox.Show("Veikkaus 9: Oikein");
+                }
+                else
+                {
+                    MessageBox.Show("Väärin");
+                }
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
