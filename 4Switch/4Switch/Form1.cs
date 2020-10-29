827fd7d27270f4b815844ce365b3cb6d89735655
JasperHolm <64134491+JasperHolm@users.noreply.github.com>
2020-10-29 11:02:22 +0200

    Add files via upload

---
+++ 4Switch/4Switch/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4Switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void nappi (string button)
+        {
+            switch (button)
+            {
+                case "1":
+                    MessageBox.Show("Valinta 1");
+                    break;
+                case "2":
+                    MessageBox.Show("Valinta 2");
+                    break;
+                case "3":
+                    MessageBox.Show("Valinta 3");
+                    break;
+                case "4":
+                    MessageBox.Show("Valinta 4");
+                    break;
+                case "Default":
+                    MessageBox.Show("Default");
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nappi("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            nappi("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nappi("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            nappi("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            nappi("Default");
+        }
+    }
+}
